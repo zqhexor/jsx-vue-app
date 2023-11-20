3562d2134f9294e20ed71e09fe79be79e32d40df
HexOr <41619463+zqhexor@users.noreply.github.com>
2023-11-20 11:56:02 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import Test1 from './components/basic/Test1.vue'
 import Test2 from './components/basic/Test2.jsx'
 import Test3 from './components/basic/Test3.vue'
+import SetupTest from './components/basic/setup-test.vue'
 import Test4 from './components/use/Test4.vue'
 import Test5 from './components/use/Test5.vue'
 import Test6 from './components/use/Test6.vue'
@@ -16,6 +17,7 @@ import Test8 from './components/use/Test8.vue'
     <Test1 msg="Hello" />
     <Test2 msg="Hello" />
     <Test3 msg="Vite + Vue" />
+    <SetupTest />
   </section>
 
   <section>
