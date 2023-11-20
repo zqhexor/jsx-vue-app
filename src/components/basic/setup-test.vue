971c3297b3a8adea17ab60a04e98ab6e0dae10c5
HexOr <41619463+zqhexor@users.noreply.github.com>
2023-11-20 11:55:21 +0800

    Create setup-test.vue
    feat: add setup example

---
+++ src/components/basic/setup-test.vue
@@ -0,0 +1,14 @@
+<template>
+  <Render />
+</template>
+
+<script setup lang="jsx">
+import { ref } from 'vue'
+
+const count = ref(1)
+const  add = () => count.value++
+
+const Render = () =>{
+  return <button onClick={add}>{count.value}</button>
+}
+</script>
